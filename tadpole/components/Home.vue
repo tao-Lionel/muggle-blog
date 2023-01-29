f303c3436abbbecb257da0fbc11bd0129d2eab6a
wangtao <lionel_tao@163.com>
2023-01-29 15:46:00 +0800

    首页

---
+++ tadpole/components/Home.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { usePageFrontmatter } from "@vuepress/client"
+
+const homeData = usePageFrontmatter().value
+console.log(homeData)
+
+const bannerBgStyle = () => {
+  return `background: url(${homeData.bannerBg}) center center / cover no-repeat`
+}
+</script>
+
+<template>
+  <div class="home-wrapper">
+    <div class="banner" :style="bannerBgStyle">
+      <header>
+        <h1>{{ homeData.heroText }}</h1>
+        <p>{{ homeData.tagline }}</p>
+      </header>
+    </div>
+    <div class="main-wrapper"></div>
+    <div class="footer"></div>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
--- tadpole/layouts/Layout.vue
+++ tadpole/layouts/Layout.vue
@@ -1,11 +1,13 @@
 <script lang="ts" setup>
-import { defineComponent, reactive, toRefs, ref } from "vue";
-import Navbar from "@/components/Navbar.vue";
+import { defineComponent, reactive, toRefs, ref } from "vue"
+import Navbar from "@/components/Navbar.vue"
+import Home from "@/components/Home.vue"
 </script>
 
 <template>
   <div class="theme-container">
     <Navbar></Navbar>
+    <Home></Home>
     <Content></Content>
   </div>
 </template>
