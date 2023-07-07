72fc23ffc655848def9b5c5bf270f526de25e92b
wangtao <lionel_tao@163.com>
2023-07-07 13:15:35 +0800

    采用vitepress构建文档

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,67 @@
+
+
+import { defineConfig } from 'vitepress';
+import AutoSidebar from 'vite-plugin-vitepress-auto-sidebar';
+
+const GIT_HUB = "https://github.com/tao-Lionel"; // github 地址
+const NAME = `muggle`; // 博主姓名
+const EMAIL = "lionel_tao@163.com";
+const BLOG_NAME = "muggle-blog";
+
+export default defineConfig({
+    base: '/',
+    title: "Muggle",
+    lang: "zh-CN",
+    description: "web前端技术博客,专注前端学习与总结。包括但不限于分享JavaScript,js,ES6,TypeScript,vue,ts,css3,css,html5,html,Node,git等技术文章。",
+
+    themeConfig: {
+        nav: [
+            {
+                text: '博客', link:'/blog/index', activeMatch: '/blog',
+            },
+            {
+                text: '笔记',link:'/note/index', activeMatch: '/note'
+            },
+            { text: '收藏夹', link: '/collect/index', },
+            { text: '关于', link: '/me/index', activeMatch: '/me' },
+        ],
+
+        // sidebar: {
+        //     '/blog': [
+        //       {
+        //         text: 'Guide',
+        //         items: [
+        //           { text: 'Index', link: '/guide/' }, // /guide/index.md
+        //           { text: 'One', link: '/guide/one' }, // /guide/one.md
+        //           { text: 'Two', link: '/guide/two' } // /guide/two.md
+        //         ]
+        //       }
+        //     ]
+        //   }
+
+        footer: {
+            message: '',
+            copyright: `${new Date().getFullYear()} Lionel tao | <a href=${GIT_HUB}/${BLOG_NAME} target="_blank">MIT License</a>`, // 博客版权信息、备案信息等，支持a标签或换行标签</br>
+          }
+    },
+    
+    vite: {
+        plugins: [
+            AutoSidebar({
+                deletePrefix : /[0-9][0-9]\./, 
+                collapsed: true,
+                ignoreIndexItem :true,
+                beforeCreateSideBarItems: (data)=>{
+                    console.log(data);
+                },
+                sideBarItemsResolved: (data)=>{
+                    // console.log(data);
+                },
+                sideBarResolved :(data)=>{
+                    // console.log(data)
+                }
+            })
+        ]
+    }
+
+});
---
+++ docs/me/index.md
@@ -0,0 +1,57 @@
+---
+title: 关于
+---
+
+## 📚Blog
+
+这是一个兼具博客文章、知识管理、文档查找的个人网站，主要内容是 Web 前端技术。
+
+:::tip
+文章内容仅是我个人的小总结，资历尚浅，如有误还请指正。
+:::
+
+## 👨‍💻Me
+
+我是一名前端，爱吃小熊饼干。上班只为下班，工资刚够早餐。头发快要掉完，前端真的好难。skr~ skr~
+
+[`更新日志`](https://github.com/tao-Lionel/muggle-blog/commits/blog)
+
+<!-- [`爱心`](/img/heart.html) -->
+
+### 💻技能
+
+- 熟悉 JavaScript、HTML、CSS、Vue 的拼写
+- 了解 Linux、windows的开关机方式
+- 精通 Git 的 pull 和 push，并注册了 GitHub 帐号刷了一些 star
+
+### 🔥爱好
+
+🏸羽毛球、🏃‍♂️跑步、📽看电影、🎮玩游戏、一切有趣好玩的事
+<!-- 本人↓↓↓
+<img src='' alt='本人照片' style="width:106px;"> -->
+
+## :email: 联系
+
+<!-- - WeChat or QQ: <a :href="qqUrl" class='qq'>{{ QQ }}</a> -->
+- Email: <a href="mailto:lionel_tao@163.com">lionel_tao@163.com</a>
+- GitHub: <https://github.com/tao-Lionel>
+
+## 🎨Theme
+
+本站主题使用了 [`Vdoing`](https://github.com/xugaoyi/vuepress-theme-vdoing)主题，这是一款简洁高效的 VuePress 知识管理&博客主题，想了解更多请点击[`详情`](https://github.com/xugaoyi/vuepress-theme-vdoing)
+<!-- <script>
+  export default {
+    data(){
+      return {
+        QQ: '894072666',
+        qqUrl: `tencent://message/?uin=${this.QQ}&Site=&Menu=yes`
+      }
+    },
+    mounted(){
+      const flag =  navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i);
+      if(flag){
+        this.qqUrl = `mqqwpa://im/chat?chat_type=wpa&uin=${this.QQ}&version=1&src_type=web&web_src=oicqzone.com`
+      }
+    }
+  }
+</script> -->
